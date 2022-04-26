cea2045f3f07405f517d69714deeb00d4a57929e
Azrinatiqah@365office.digital <Azrinatiqah@365office.digital>
2022-04-27 00:43:37 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.cbSrate = New System.Windows.Forms.ComboBox()
+        Me.cbYear = New System.Windows.Forms.ComboBox()
+        Me.cbErate = New System.Windows.Forms.ComboBox()
+        Me.txtLoan = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.ListBox()
+        Me.btnList = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtRate = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(324, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CAR LOAN PAYMENT CALCULATOR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Loan Calculator"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Loan Amount:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Start Rate (%):"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 151)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Rate Step Size:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(305, 79)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Years:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(305, 114)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 15)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "End Rates:"
+        '
+        'cbSrate
+        '
+        Me.cbSrate.FormattingEnabled = True
+        Me.cbSrate.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cbSrate.Location = New System.Drawing.Point(114, 111)
+        Me.cbSrate.Name = "cbSrate"
+        Me.cbSrate.Size = New System.Drawing.Size(121, 23)
+        Me.cbSrate.TabIndex = 7
+        '
+        'cbYear
+        '
+        Me.cbYear.FormattingEnabled = True
+        Me.cbYear.Items.AddRange(New Object() {"2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cbYear.Location = New System.Drawing.Point(372, 70)
+        Me.cbYear.Name = "cbYear"
+        Me.cbYear.Size = New System.Drawing.Size(121, 23)
+        Me.cbYear.TabIndex = 9
+        '
+        'cbErate
+        '
+        Me.cbErate.FormattingEnabled = True
+        Me.cbErate.Items.AddRange(New Object() {"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
+        Me.cbErate.Location = New System.Drawing.Point(372, 108)
+        Me.cbErate.Name = "cbErate"
+        Me.cbErate.Size = New System.Drawing.Size(121, 23)
+        Me.cbErate.TabIndex = 10
+        '
+        'txtLoan
+        '
+        Me.txtLoan.Location = New System.Drawing.Point(115, 71)
+        Me.txtLoan.Name = "txtLoan"
+        Me.txtLoan.Size = New System.Drawing.Size(121, 23)
+        Me.txtLoan.TabIndex = 11
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(115, 187)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 12
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(209, 187)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.FormattingEnabled = True
+        Me.lblOutput.ItemHeight = 15
+        Me.lblOutput.Location = New System.Drawing.Point(70, 216)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(423, 94)
+        Me.lblOutput.TabIndex = 14
+        '
+        'btnList
+        '
+        Me.btnList.Location = New System.Drawing.Point(434, 339)
+        Me.btnList.Name = "btnList"
+        Me.btnList.Size = New System.Drawing.Size(75, 23)
+        Me.btnList.TabIndex = 15
+        Me.btnList.Text = "Carlist"
+        Me.btnList.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblName.Location = New System.Drawing.Point(12, 335)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(401, 39)
+        Me.lblName.TabIndex = 16
+        '
+        'txtRate
+        '
+        Me.txtRate.Location = New System.Drawing.Point(117, 158)
+        Me.txtRate.Name = "txtRate"
+        Me.txtRate.Size = New System.Drawing.Size(119, 23)
+        Me.txtRate.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(545, 383)
+        Me.Controls.Add(Me.txtRate)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnList)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtLoan)
+        Me.Controls.Add(Me.cbErate)
+        Me.Controls.Add(Me.cbYear)
+        Me.Controls.Add(Me.cbSrate)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents cbSrate As ComboBox
+    Friend WithEvents cbYear As ComboBox
+    Friend WithEvents cbErate As ComboBox
+    Friend WithEvents txtLoan As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblOutput As ListBox
+    Friend WithEvents btnList As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtRate As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim strAmount As Double = CDbl(txtLoan.Text)
+        Dim strYears As Integer = (cbYear.SelectedItem)
+        Dim strsRate As Integer = (cbSrate.SelectedItem)
+        Dim strErate As Integer = (cbErate.SelectedItem)
+        Dim strStepR As Double = CDbl(txtRate.Text)
+        Dim result As Decimal
+        Dim rate As Double
+        Dim arrange As String = "{0}                  {1}"
+        Dim month As Integer = strYears * 12
+
+        strAmount = Double.Parse(txtLoan.Text.ToString())
+        strYears = Double.Parse(cbYear.GetItemText(cbYear.SelectedItem))
+        strsRate = Double.Parse(cbSrate.GetItemText(cbSrate.SelectedItem))
+        strErate = Double.Parse(cbErate.GetItemText(cbErate.SelectedItem))
+        strStepR = Double.Parse(txtRate.Text.ToString())
+
+        For rate = strsRate To strErate Step strStepR
+            'Interest rate = (rate / 12) / 100
+            result = -Pmt((rate / 12) / 100, month, strAmount)
+            lblOutput.Items.Add(String.Format(arrange, FormatPercent(rate.ToString / 100), FormatCurrency(result.ToString, 2)))
+
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim strName As String
+        lblName.Text = "Name: NUR AZRINA ATIQAH BINTI AZRI (10DDT20F1030)"
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtLoan.Text = ""
+        txtRate.Text = ""
+        cbErate.ResetText()
+        cbSrate.ResetText()
+        cbYear.ResetText()
+        lblOutput.Items.Clear()
+    End Sub
+
+    Private Sub btnList_Click(sender As Object, e As EventArgs) Handles btnList.Click
+        MsgBox("
+         Perodua Ativa
+         5-Seater SUV
+         From RM61,500
+
+         Proton X50
+         SUV
+         From RM79,200
+
+         Honda HR-V 
+         Compact crossover SUV
+         From RM104,000
+
+         Nissan X-Trail
+         Compact crossover SUV
+         From RM128,630", MsgBoxStyle.OkCancel, "Car List")
+    End Sub
+End Class
+
